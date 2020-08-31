728070d08ffc870bd9471209e33cdf74b50f62f2
Nora <nora.hamacher@gmail.com>
2020-08-31 10:20:50 +1000

    Updates

---
+++ precache-manifest.1fdef799f17f1fd7e6f673f5ec427005.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3104700fbfb1802652f053df21ba53",
+    "url": "/superdiversity/index.html"
+  },
+  {
+    "revision": "1fdeb490da7deb4e8a96",
+    "url": "/superdiversity/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "b47bdd6ed4f62947a78d",
+    "url": "/superdiversity/static/css/main.a7aff635.chunk.css"
+  },
+  {
+    "revision": "1fdeb490da7deb4e8a96",
+    "url": "/superdiversity/static/js/2.5287c764.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/superdiversity/static/js/2.5287c764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47bdd6ed4f62947a78d",
+    "url": "/superdiversity/static/js/main.0fc721e7.chunk.js"
+  },
+  {
+    "revision": "9fb42bdd81d000d13cdc",
+    "url": "/superdiversity/static/js/runtime-main.1b71433b.js"
+  },
+  {
+    "revision": "a9095b3d11aae88c299d9f6b0c00e420",
+    "url": "/superdiversity/static/media/bivariate_scheme1.a9095b3d.png"
+  }
+]);
